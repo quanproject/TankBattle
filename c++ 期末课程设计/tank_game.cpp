404ae95ba9c31b207e8002ad6b879da3be9ddc9f
quanproject <45907211+quanproject@users.noreply.github.com>
2018-12-17 18:24:25 +0800

    1

--- c++ 期末课程设计/tank_game.cpp
+++ c++ 期末课程设计/tank_game.cpp
@@ -1,8 +1,25 @@
-﻿#include<iostream>
-#include<stdlib.h>
+﻿#include"tank_game.h"
 using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++主函数+++++++++++++++++++++++++++++++++++++++++++++//
 int main()
 {
+	initgraph(1200, 900);//初始化图形界面
 	cout << "success！" << endl;
 	system("pause");
 	return 0;
--- c++ 期末课程设计/tank_game.h
+++ c++ 期末课程设计/tank_game.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include<iostream>
+#include<stdlib.h>
+#include<graphics.h>     //easyX图形库
+#include<conio.h>
+#include<time.h>
+#include<windows.h>
+
+//常量定义
+const int SIZE = 30;       //绘图使用的标准格数（一格为30像素
+const int TANK = 1;        //坦克
+const int HOME = 2;        //家
+const int SODWALL = 5;     //土墙 易破坏的墙
+const int STEEWALL = 6;    //铁墙 不易破坏的墙
+const int BRUSH = 7;       //灌木 坦克可以穿越 炮弹无法穿过
+const int RIVER = 8;       //河流 坦克不能穿越 炮弹可以穿越
