5f6374f18bdfa92f0c0b85a1dfa32ec2fb64da6b
quanproject <45907211+quanproject@users.noreply.github.com>
2018-12-17 22:01:42 +0800

    菜单类

---
+++ c++ 期末课程设计/TankGameMenu.h
@@ -0,0 +1,12 @@
+#pragma once
+#include"main_game.h"
+class Menu
+{
+public:
+	void printmenu();           //打印开始界面
+	Menu();
+	~Menu();
+private:
+
+};
+
--- c++ 期末课程设计/main_game.cpp
+++ c++ 期末课程设计/main_game.cpp
@@ -20,6 +20,7 @@ using namespace std;
 int main()
 {
 	initgraph(1200, 900);//初始化图形界面
+
 	cout << "success！" << endl;
 	system("pause");
 	return 0;
